d074a00d388bb3d5a4241943b4b13a5a64248e6a
nosa <no@winkler-software.io>
2022-03-26 19:05:40 +0100

    added colors

---
+++ frontend/lib/utils/speaq_styles.dart
@@ -0,0 +1,21 @@
+//region Colors
+import 'dart:ui';
+
+const Color speaqPrimaryBlue = Color(0xFF3388DD);
+const Color speaqPrimaryBlueTranslucent = Color(0x773388DD);
+const Color speaqSecondaryAqua = Color(0xFF11CCFF);
+const Color speaqSecondaryAquaTranslucent = Color(0x7711CCFF);
+const Color speaqWarningOrange = Color(0xFFFF9944);
+const Color speaqWarningOrangeTranslucent = Color(0x77FF9944);
+const Color speaqErrorRed = Color(0xFFFF1111);
+const Color speaqErrorRedTranslucent = Color(0x77FF1111);
+
+const Color speaqWhite = Color(0xFFF7F7F7);
+const Color speaqWhiteTranslucent = Color(0x77F7F7F7);
+const Color speaqLightGrey = Color(0xFFCCCCCC);
+const Color speaqLightGreyTranslucent = Color(0x77CCCCCC);
+const Color speaqDarkGrey = Color(0xFF777777);
+const Color speaqDarkGreyTranslucent = Color(0x77777777);
+const Color speaqBlack = Color(0xFF080808);
+const Color speaqBlackTranslucent = Color(0x77080808);
+//endregion
