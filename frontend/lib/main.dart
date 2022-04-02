22b578d0dc279076046dd0d93c4bde2a521bf875
Nosakhare Omoruyi <nomoruyi@stud.hs-heilbronn.de>
2022-04-02 15:27:24 +0200

    Settings package added

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -14,21 +14,21 @@ class Speaq extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(foregroundColor: spqBlack, backgroundColor: spqWhite),
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(foregroundColor: spqBlack, backgroundColor: spqWhite),
           scaffoldBackgroundColor: spqWhite,
           backgroundColor: spqWhite,
           bottomAppBarColor: spqWhite,
-          bottomNavigationBarTheme:  const BottomNavigationBarThemeData(backgroundColor: spqWhite, selectedItemColor: spqPrimaryBlue, unselectedItemColor: spqDarkGrey ),
+          bottomNavigationBarTheme:
+              const BottomNavigationBarThemeData(backgroundColor: spqWhite, selectedItemColor: spqPrimaryBlue, unselectedItemColor: spqDarkGrey),
           dialogBackgroundColor: spqWhite,
           primaryColor: spqPrimaryBlue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
           errorColor: spqErrorRed,
           shadowColor: spqLightGreyTranslucent,
           //MOCKUP-SCHRIFTART (POPPINS) ALS STANDARDFONT
-          textTheme: spqTextTheme
-      ),
-      initialRoute: "login",
+          textTheme: spqTextTheme),
+      initialRoute: "settings",
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
@@ -42,7 +42,6 @@ class MainApp extends StatelessWidget {
     return Container();
   }
 }
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
--- frontend/lib/pages/settings/settings_page.dart
+++ frontend/lib/pages/settings/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -9,7 +10,30 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   @override
+  initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SettingsScreen(
+        children: [
+          SettingsGroup(title: "First Settings Group", children: [
+            SimpleSettingsTile(
+              title: "Settings Tile",
+              child: Text("H"),
+              onTap: () => print("hh"),
+            ),
+            SwitchSettingsTile(title: "Switch Tile", settingKey: "switch_key"),
+          ]),
+          SettingsGroup(title: "Second Settings Group", children: [
+            CheckboxSettingsTile(title: "Checkbox Tile", settingKey: "checkbox_key"),
+            DropDownSettingsTile(title: "Drop Down Tile", settingKey: "drop_down_key", selected: 0, values: {0: "Null", 1: "Eins", 2: "Zwei"}),
+            SliderSettingsTile(title: "Slider Tile", settingKey: "slider_key", min: 0, max: 4),
+          ])
+        ],
+      ),
+    );
   }
 }
