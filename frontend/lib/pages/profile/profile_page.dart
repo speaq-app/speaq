8d053fc1af4014b434b73e709614f715dc5106e5
dloewe <dloewe@stud.hs-heilbronn.de>
2022-04-08 17:11:17 +0200

    feat: extracted widgets, code cleanup

--- frontend/lib/pages/profile/profile_page.dart
+++ frontend/lib/pages/profile/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/utils/speaq_styles.dart';
-
+import 'package:frontend/widgets/speaq_post_container.dart';
+import 'package:frontend/widgets/speaq_text_button.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,6 +11,15 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final String _postMessage = "Welcome to our presentation, how are you ? Just did something lit here!!! yeah #speaq #beer ";
+  final String _link = "hs-heilbronn.de";
+  final String _name = "Informatics";
+  final String _username = "@hhn";
+  final String _bio = "I like Hochschule Heilbronn";
+  final String _joined = "Joined August 2022";
+  final String _follower = "117k Follower";
+  final String _following = "69 Follower";
+
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
@@ -24,23 +34,22 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  SizedBox _buildHeader(Size deviceSize, BuildContext context) =>
-      SizedBox(
-          width: deviceSize.width,
-          height: deviceSize.height * 0.50,
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Positioned(top: 0, child: _buildCover(deviceSize)),
-              Positioned(
-                  top: deviceSize.height * 0.13,
-                  height: deviceSize.height * 0.17,
-                  width: deviceSize.width,
-                  child: _buildPictureEditProfile(deviceSize)),
-              Positioned(
-                  top: 210, child: _buildProfileInformation(context, deviceSize)),
-            ],
-          ));
+  SizedBox _buildHeader(Size deviceSize, BuildContext context) => SizedBox(
+      width: deviceSize.width,
+      height: deviceSize.height * 0.50,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(top: 0, child: _buildCover(deviceSize)),
+          Positioned(
+              top: deviceSize.height * 0.13,
+              height: deviceSize.height * 0.17,
+              width: deviceSize.width,
+              child: _buildPictureEditProfile(deviceSize)),
+          Positioned(
+              top: 210, child: _buildProfileInformation(context, deviceSize)),
+        ],
+      ));
 
   Widget _buildCover(Size deviceSize) {
     return Container(
@@ -65,8 +74,10 @@ class _ProfilePageState extends State<ProfilePage> {
           child: _buildProfilePicture(deviceSize),
         ),
         Padding(
-          padding:  EdgeInsets.only(right: 24.0,top: deviceSize.height * 0.09),
-          child: SpqTextButton(onPressed: reset,),
+          padding: EdgeInsets.only(right: 24.0, top: deviceSize.height * 0.09),
+          child: SpqTextButton(
+            onPressed: reset,
+          ),
         )
       ],
     );
@@ -74,15 +85,13 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget _buildProfilePicture(Size deviceSize) {
     return GestureDetector(
-      onTap: () =>
-          Navigator.of(context).push(PageRouteBuilder(
-            pageBuilder: (context, animation, secondaryAnimation) =>
-                _buildProfileImageFullScreen(context),
-            transitionsBuilder: (context, animation, secondaryAnimation,
-                child) {
-              return child;
-            },
-          )),
+      onTap: () => Navigator.of(context).push(PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            _buildProfileImageFullScreen(context),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return child;
+        },
+      )),
       child: const Hero(
         tag: 'myImage',
         child: CircleAvatar(
@@ -102,47 +111,51 @@ class _ProfilePageState extends State<ProfilePage> {
         height: deviceSize.height * 0.3,
         padding: const EdgeInsets.only(top: 10),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          const Text('Corinna',
-              style: TextStyle(
-                  color: spqBlack,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30)),
-          const Text('@corinna',
-              style: TextStyle(
+          Text(_name,
+              style: const TextStyle(
+                  color: spqBlack, fontWeight: FontWeight.bold, fontSize: 23)),
+          Text(_username,
+              style: const TextStyle(
                 color: spqDarkGrey,
                 fontSize: 18,
               )),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 6.0),
-            child: Text('I like big America',
-                style: TextStyle(color: spqBlack, fontSize: 18)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6.0),
+            child: Text(_bio,
+                style: const TextStyle(color: spqBlack, fontSize: 19)),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: Row(
-              children: const [
-                Icon(Icons.link),
-                Text('@corinna.io', style: TextStyle(color: spqPrimaryBlue)),
-                SizedBox(width: 15),
-                Icon(Icons.calendar_month),
-                Text('Joined September 2018',
-                    style: TextStyle(color: spqDarkGrey))
+              children: [
+                const Icon(Icons.link),
+                Text(_link,
+                    style:
+                        const TextStyle(color: spqPrimaryBlue, fontSize: 16)),
+                const SizedBox(width: 15),
+                const Icon(Icons.calendar_month),
+                Text(_joined,
+                    style: const TextStyle(color: spqDarkGrey, fontSize: 16))
               ],
             ),
           ),
           Row(
             mainAxisSize: MainAxisSize.min,
-            children: const [
+            children: [
               Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Text('21 Following',
-                    style: TextStyle(
-                        color: spqBlack, fontWeight: FontWeight.bold)),
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(_following,
+                    style: const TextStyle(
+                        color: spqBlack,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16)),
               ),
-              SizedBox(width: 25),
-              Text('117k Followers',
-                  style: TextStyle(
-                      color: spqBlack, fontWeight: FontWeight.bold)),
+              const SizedBox(width: 25),
+              Text(_follower,
+                  style: const TextStyle(
+                      color: spqBlack,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16)),
             ],
           ),
         ]),
@@ -175,7 +188,10 @@ class _ProfilePageState extends State<ProfilePage> {
           body: Padding(
             padding: const EdgeInsets.symmetric(vertical: 7.0),
             child: TabBarView(
-              children: [listViewPostText(deviceSize), listViewPostText(deviceSize)],
+              children: [
+                listViewPostText(deviceSize),
+                listViewPostText(deviceSize)
+              ],
             ),
           ),
         ),
@@ -184,21 +200,21 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget listViewPostText(Size deviceSize) {
-    return Column(children: const [
-      PostContainer(),
-      Divider(thickness: 0.5, color: spqLightGreyTranslucent),
-      PostContainer(),
-      Divider(thickness: 0.5, color: spqLightGreyTranslucent),
-      PostContainer()
+    return Column(children: [
+      PostContainer(name: _name, username: _username, postMessage: _postMessage),
+      const Divider(thickness: 0.55, color: spqLightGreyTranslucent),
+      PostContainer(name: _name, username: _username, postMessage: _postMessage),
+      const Divider(thickness: 0.55, color: spqLightGreyTranslucent),
+      PostContainer(name: _name, username: _username, postMessage: _postMessage)
     ]);
   }
 
   Widget _buildProfileImageFullScreen(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "@corinna",
-          style: TextStyle(color: spqBlack),
+        title: Text(
+          _username,
+          style: const TextStyle(color: spqBlack),
         ),
         centerTitle: true,
         leading: const BackButton(
@@ -220,93 +236,4 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   void reset() {}
-}
-
-class PostContainer extends StatelessWidget {
-  const PostContainer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          leading: const CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://www.jobvector.de/karriere-ratgeber/wp-content/uploads/2021/05/it-security360x240.jpg'),
-          ),
-          title: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Text("Corinna Kopf",
-                      style:
-                      TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15.0),
-                    child: Text("@Corinna",
-                        style: TextStyle(fontSize: 15, color: spqDarkGrey)),
-                  ),
-                  Text("12/39/19",
-                      style: TextStyle(fontSize: 15, color: spqDarkGrey))
-                ],
-              )
-            ],
-          ),
-          subtitle: Column(
-            children: [
-              const Text(
-                "Hello and Welcome to  America lover Corinna, very nice to see you guys,"
-                    "Download: i.dont.know.this.link.io #beer#beer#beer#beer#beer#beer",
-                overflow: TextOverflow.clip,
-                style: TextStyle(color: spqBlack, fontSize: 18),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  Icon(Icons.mic, color: spqDarkGrey),
-                  Text("69"),
-                  SizedBox(width: 30),
-                  Icon(Icons.favorite, color: spqErrorRed),
-                  Text("238"),
-                  SizedBox(width: 30),
-                  Icon(Icons.ios_share, color: spqDarkGrey),
-                  SizedBox(width: 30),
-                  Icon(Icons.bookmark, color: spqDarkGrey)
-                ],
-              )
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class SpqTextButton extends StatelessWidget {
-  const SpqTextButton({Key? key,void function, required this.onPressed}) : super(key: key);
-
-  final void Function() onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => onPressed,
-      child: const Text(
-        "Edit Profile",
-        style: TextStyle(color: spqPrimaryBlue),
-      ),
-      style: ElevatedButton.styleFrom(
-        primary: spqWhite,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            side: BorderSide(
-              color: spqPrimaryBlue,
-            )),
-      ),
-    );
-  }
 }
