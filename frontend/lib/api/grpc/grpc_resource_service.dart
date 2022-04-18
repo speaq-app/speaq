2e2c57936f1aa031f2515ead71090c559e5263e4
Hendrik Schlehlein <hendrik.schlehlein@gmail.com>
2022-04-18 15:44:09 +0200

    fix: api client

--- frontend/lib/api/grpc/grpc_resource_service.dart
+++ frontend/lib/api/grpc/grpc_resource_service.dart
@@ -4,14 +4,22 @@ import 'package:frontend/api/model/resource.dart';
 import 'package:frontend/api/resource_service.dart';
 import 'package:grpc/grpc.dart';
 
-abstract class GRPCResourceService implements ResourceService {
-  static final ResourceClient _client = ResourceClient(
-    ClientChannel(
-      "10.0.2.2",
-      port: 8080,
-      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
-    ),
-  );
+class GRPCResourceService implements ResourceService {
+  late ResourceClient _client;
+
+  GRPCResourceService(
+    String ip, {
+    int port = 443,
+  }) {
+    _client = ResourceClient(
+      ClientChannel(
+        ip,
+        port: port,
+        options:
+            const ChannelOptions(credentials: ChannelCredentials.insecure()),
+      ),
+    );
+  }
 
   @override
   Future<Resource> getResource(int id) async {
--- frontend/lib/api/resource_service.dart
+++ frontend/lib/api/resource_service.dart
@@ -1,9 +1,5 @@
 import 'package:frontend/api/model/resource.dart';
 
 abstract class ResourceService {
-  Future<Resource> getResource(int id) async {
-    return Future.delayed(
-      const Duration(seconds: 1),
-    );
-  }
+  Future<Resource> getResource(int id);
 }
