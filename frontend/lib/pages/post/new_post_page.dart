96fb36fedbff2d3d27e41602e87dd3f5f3a8cb14
Nosakhare Omoruyi <97680892+nomoruyi@users.noreply.github.com>
2022-05-02 20:54:02 +0200

    feat: SWPG6SPEAK-183/184/164-post-speaq-page (#37)
    * feat: add backend implementation
    
    * feat(new post page): #164, #183, #184
    
    * ...
    
    Co-authored-by: Hendrik Schlehlein <hendrik.schlehlein@gmail.com>
    Co-authored-by: nomoruyi <nomoruyi@stud.hs-heilbronn.de>

--- frontend/lib/pages/post/new_post_page.dart
+++ frontend/lib/pages/post/new_post_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/utils/all_utils.dart';
 import 'package:frontend/widgets/all_widgets.dart';
 
 class NewPostPage extends StatefulWidget {
@@ -9,10 +13,124 @@ class NewPostPage extends StatefulWidget {
 }
 
 class _NewPostPageState extends State<NewPostPage> {
+  final TextEditingController _postController = TextEditingController();
+  bool emojiShowing=false;
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
+    AppLocalizations appLocale = AppLocalizations.of(context)!;
 
-    return SafeArea(child: Scaffold(appBar: SpqAppBar(title: const Text("Speaq"), preferredSize: deviceSize,),));
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: SpqAppBar(
+          preferredSize: deviceSize,
+          actionList: [_buildSendPostButton()],
+        ),
+        body: _buildPostTextField(appLocale),
+      ),
+    );
+  }
+
+  Widget _buildPostTextField(AppLocalizations appLocale) => Padding(padding: const EdgeInsets.all(8.0),child: SpqPostTextField(height: double.infinity, minLines: 30,controller: _postController, hintText: appLocale.newPost,));
+
+  Widget _buildSendPostButton() => TextButton(onPressed: ()=> print("Speaq"), child: Container(child: const Text("speaq"),margin: const EdgeInsets.symmetric(horizontal: 4.0),padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),decoration: BoxDecoration(border: Border.all(color: spqPrimaryBlue, width: 1.0),borderRadius: const BorderRadius.all(Radius.circular(16.0))),));
+}
+
+class SpqPostTextField extends StatelessWidget {
+  const SpqPostTextField({
+    Key? key,
+    required this.controller,
+    this.obscureText = false,
+    this.isPassword = false,
+    required this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onTap,
+    this.validator,
+    this.keyboardType,
+    this.minLines,
+    this.maxLines,
+    this.width,
+    this.height = 56,
+    this.contentPadding = const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+    this.enabled = true,
+  }) : super(key: key);
+
+  final TextEditingController? controller;
+  final bool obscureText;
+  final bool isPassword;
+  final String hintText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final void Function()? onTap;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final int? minLines;
+  final int? maxLines;
+  final double? width;
+  final double? height;
+  final EdgeInsets? contentPadding;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextFormField(
+        textAlign: TextAlign.start,
+        textAlignVertical: TextAlignVertical.center,
+        minLines: minLines,
+        maxLines: maxLines,
+        obscureText: obscureText,
+        readOnly: isPassword,
+        keyboardType: keyboardType,
+        enableSuggestions: !isPassword,
+        autocorrect: !isPassword,
+        controller: controller,
+        validator: (value) => validator!(value),
+        style: const TextStyle(color: spqBlack, fontSize: 16),
+        enabled: enabled,
+        decoration: InputDecoration(
+          isDense: true,
+          label: Container(
+            margin: const EdgeInsets.only(bottom: 12.0),
+            child: Text(hintText, style: const TextStyle(color: spqLightGrey, fontWeight: FontWeight.w100)),
+          ),
+          contentPadding: contentPadding,
+          labelStyle: const TextStyle(color: spqLightGrey, fontWeight: FontWeight.w100),
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          floatingLabelAlignment: FloatingLabelAlignment.start,
+          alignLabelWithHint: true,
+          prefixIcon: prefixIcon != null
+              ? Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: prefixIcon,
+          )
+              : null,
+          suffixIcon: suffixIcon,
+          fillColor: spqWhite,
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: spqPrimaryBlue, width: 1.0),
+          ),
+          hintStyle: const TextStyle(color: spqLightGrey, fontSize: 16, fontWeight: FontWeight.w100),
+          //hintText: hintText,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: spqWhite, width: 1.0),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: spqWhite, width: 1.0),
+          ),
+          border: const OutlineInputBorder(),
+        ),
+        onTap: onTap,
+      ),
+    );
   }
 }
+
