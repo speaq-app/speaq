717a3cf4f87c0e241caab8da52ea29bf77d74787
Hendrik Schlehlein <hendrik.schlehlein@gmail.com>
2022-05-02 16:42:19 +0000

    feat: SWPG6SPEAK-152: add imprint backend implementation (#33)
    * feat: add backend implementation
    
    * refactor: make build

---
+++ backend/internal/app/settings/settings_test.go
@@ -0,0 +1,42 @@
+package settings_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/speaq-app/speaq/internal/app/settings"
+)
+
+func TestGetResource(t *testing.T) {
+	tt := []struct {
+		name       string
+		imprintURL string
+	}{
+		{
+			name:       "NoImprintURL",
+			imprintURL: "",
+		},
+		{
+			name:       "ExampleImprintURL",
+			imprintURL: "https://example.com/imprint",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			srv := settings.Server{
+				ImprintURL: tc.imprintURL,
+			}
+
+			resp, err := srv.GetImprint(context.Background(), &empty.Empty{})
+			if err != nil {
+				t.Error(err)
+			}
+
+			if resp.Url != tc.imprintURL {
+				t.Fail()
+			}
+		})
+	}
+}
