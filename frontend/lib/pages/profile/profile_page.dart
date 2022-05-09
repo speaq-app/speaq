8f03fd29214570c2af3da621114a574cb30e3143
dloewe <dloewe@stud.hs-heilbronn.de>
2022-05-09 23:04:45 +0200

    fix: profile-Page

--- frontend/lib/pages/profile/profile_page.dart
+++ frontend/lib/pages/profile/profile_page.dart
@@ -45,8 +45,9 @@ class _ProfilePageState extends State<ProfilePage> {
           physics: const BouncingScrollPhysics(
               parent: AlwaysScrollableScrollPhysics()),
           children: [
-            _buildHeader(deviceSize, context),
-            _buildTabs(deviceSize),
+            _buildProfileCover(deviceSize, context),
+            _buildProfileStack(deviceSize),
+            _buildTabs(deviceSize)
           ],
         ),
         bottomNavigationBar: SpqButtonNavigationBar(
@@ -56,91 +57,6 @@ class _ProfilePageState extends State<ProfilePage> {
           selectedIndex: 0,
         ),
       ),
-    );
-  }
-
-  SizedBox _buildHeader(Size deviceSize, BuildContext context) => SizedBox(
-        width: deviceSize.width,
-        height: deviceSize.height * 0.50,
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              top: 0,
-              child: _buildCover(deviceSize),
-            ),
-            Positioned(
-              top: deviceSize.height * 0.13,
-              height: deviceSize.height * 0.17,
-              width: deviceSize.width,
-              child: _buildPictureEditProfile(deviceSize),
-            ),
-            Positioned(
-              top: 210,
-              child: _buildProfileInformation(context, deviceSize),
-            ),
-          ],
-        ),
-      );
-
-  Widget _buildCover(Size deviceSize) {
-    return Container(
-      height: deviceSize.height * 0.225,
-      width: deviceSize.width,
-      decoration: const BoxDecoration(
-        color: spqPrimaryBlue,
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: NetworkImage(
-              'https://www.jobvector.de/karriere-ratgeber/wp-content/uploads/2021/05/it-security360x240.jpg'),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPictureEditProfile(Size deviceSize) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 24.0),
-          child: _buildProfilePicture(deviceSize),
-        ),
-        Padding(
-          padding: EdgeInsets.only(right: 24.0, top: deviceSize.height * 0.09),
-          child: SpqTextbutton(
-              onPressed: () => Navigator.pushNamed(context, 'edit_profile'),
-              name: _editProfile,
-              style: const TextStyle(color: spqPrimaryBlue)),
-        ),
-/*        Padding(
-          padding: EdgeInsets.only(right: 24.0, top: deviceSize.height * 0.09),
-          child: isFollow == true
-               ?SpqTextbutton(
-                  onPressed: () {
-                    setState(
-                      () {
-                        isFollow = false;
-                      },
-                    );
-                  },
-                  name: _unfollow,
-                  style: const TextStyle(color:  spqErrorRed),
-                )
-              : SpqTextbutton(
-                  onPressed: () {
-                    setState(
-                      () {
-                        isFollow = true;
-                      },
-                    );
-                  },
-                  name: _follow,
-                 style: const TextStyle(color:spqPrimaryBlue ),
-                ),
-        )*/
-      ],
     );
   }
 
@@ -166,82 +82,208 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  Widget _buildProfileInformation(BuildContext context, Size deviceSize) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: Container(
+  Widget _buildProfileImageFullScreen(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _username,
+          style: const TextStyle(color: spqBlack),
+        ),
+        centerTitle: true,
+        leading: const BackButton(
+          color: spqBlack,
+        ),
+        backgroundColor: spqWhite,
+      ),
+      body: Container(
+        color: spqWhite,
+        child: Center(
+          child: Hero(
+            tag: 'myImage',
+            child: Image.network(
+                "https://unicheck.unicum.de/sites/default/files/artikel/image/informatik-kannst-du-auch-auf-englisch-studieren-gettyimages-rosshelen-uebersichtsbild.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileCover(Size deviceSize, BuildContext context) {
+    return Container(
+      height: deviceSize.height * 0.225,
+      width: deviceSize.width,
+      decoration: const BoxDecoration(
+        color: spqPrimaryBlue,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              'https://www.jobvector.de/karriere-ratgeber/wp-content/uploads/2021/05/it-security360x240.jpg'),
+        ),
+      ),
+    );
+  }
+
+  /*SizedBox _buildHeader(Size deviceSize, BuildContext context) => SizedBox(
         width: deviceSize.width,
-        height: deviceSize.height * 0.3,
-        padding: const EdgeInsets.only(top: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        height: deviceSize.height * 0.50,
+        child: Stack(
+          clipBehavior: Clip.none,
           children: [
-            Text(
-              _name,
-              style: const TextStyle(
-                  color: spqBlack, fontWeight: FontWeight.bold, fontSize: 23),
+            Positioned(
+              top: 0,
+              child: _buildCover(deviceSize),
             ),
-            Text(
-              _username,
-              style: const TextStyle(
-                color: spqDarkGrey,
-                fontSize: 18,
-              ),
+            Positioned(
+              top: deviceSize.height * 0.13,
+              height: deviceSize.height * 0.17,
+              width: deviceSize.width,
+              child: _buildPictureEditProfile(deviceSize),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 6.0),
-              child: Text(
-                _bio,
-                style: const TextStyle(color: spqBlack, fontSize: 19),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5.0),
-              child: Row(
-                children: [
-                  const Icon(Icons.link),
-                  Text(
-                    _link,
-                    style: const TextStyle(color: spqPrimaryBlue, fontSize: 16),
-                  ),
-                  const SizedBox(width: 15),
-                  const Icon(Icons.calendar_month),
-                  Text(
-                    _joined,
-                    style: const TextStyle(color: spqDarkGrey, fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-            InkWell(
-              onTap: () => Navigator.pushNamed(context, 'follow'),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      _following,
-                      style: const TextStyle(
-                          color: spqBlack,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
-                  ),
-                  const SizedBox(width: 25),
-                  Text(
-                    _follower,
-                    style: const TextStyle(
-                        color: spqBlack,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                  ),
-                ],
-              ),
+            Positioned(
+              top: 210,
+              child: _buildProfileInformation(context, deviceSize),
             ),
           ],
         ),
+      );*/
+
+/*  Widget _buildCover(Size deviceSize) {
+    return Container(
+      height: deviceSize.height * 0.225,
+      width: deviceSize.width,
+      decoration: const BoxDecoration(
+        color: spqPrimaryBlue,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              'https://www.jobvector.de/karriere-ratgeber/wp-content/uploads/2021/05/it-security360x240.jpg'),
+        ),
       ),
+    );
+  }*/
+
+  Widget _buildProfileStack(Size deviceSize) {
+    return Container(
+      transform: Matrix4.translationValues(0, -40, 0),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              _buildProfilePicture(deviceSize),
+              Container(
+                width: deviceSize.width * 0.31,
+                height: deviceSize.height * 0.05,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SpqTextbutton(
+                  onPressed: () => Navigator.pushNamed(context, 'edit_profile'),
+                  name: _editProfile,
+                  style: const TextStyle(color: spqPrimaryBlue),
+                ),
+              ),
+/*        Padding(
+                    padding: EdgeInsets.only(right: 24.0, top: deviceSize.height * 0.09),
+                    child: isFollow == true
+                         ?SpqTextbutton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  isFollow = false;
+                                },
+                              );
+                            },
+                            name: _unfollow,
+                            style: const TextStyle(color:  spqErrorRed),
+                          )
+                        : SpqTextbutton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  isFollow = true;
+                                },
+                              );
+                            },
+                            name: _follow,
+                           style: const TextStyle(color:spqPrimaryBlue ),
+                          ),
+                  )*/
+            ],
+          ),
+          _buildProfileInformation(context, deviceSize),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileInformation(BuildContext context, Size deviceSize) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          _name,
+          style: const TextStyle(
+              color: spqBlack, fontWeight: FontWeight.bold, fontSize: 23),
+        ),
+        Text(
+          _username,
+          style: const TextStyle(
+            color: spqDarkGrey,
+            fontSize: 18,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6.0),
+          child: Text(
+            _bio,
+            style: const TextStyle(color: spqBlack, fontSize: 19),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0),
+          child: Row(
+            children: [
+              const Icon(Icons.link),
+              Text(
+                _link,
+                style: const TextStyle(color: spqPrimaryBlue, fontSize: 16),
+              ),
+              const SizedBox(width: 15),
+              const Icon(Icons.calendar_month),
+              Text(
+                _joined,
+                style: const TextStyle(color: spqDarkGrey, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () => Navigator.pushNamed(context, 'follow'),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  _following,
+                  style: const TextStyle(
+                      color: spqBlack,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+              ),
+              const SizedBox(width: 25),
+              Text(
+                _follower,
+                style: const TextStyle(
+                    color: spqBlack, fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
@@ -271,8 +313,8 @@ class _ProfilePageState extends State<ProfilePage> {
             padding: const EdgeInsets.symmetric(vertical: 7.0),
             child: TabBarView(
               children: [
-                listViewPostText(deviceSize),
-                listViewPostText(deviceSize)
+                listViewPostText(),
+                listViewPostText(),
               ],
             ),
           ),
@@ -281,7 +323,7 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  Widget listViewPostText(Size deviceSize) {
+  Widget listViewPostText() {
     return Column(
       children: [
         PostContainer(
@@ -293,32 +335,6 @@ class _ProfilePageState extends State<ProfilePage> {
         PostContainer(
             name: _name, username: _username, postMessage: _postMessage),
       ],
-    );
-  }
-
-  Widget _buildProfileImageFullScreen(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          _username,
-          style: const TextStyle(color: spqBlack),
-        ),
-        centerTitle: true,
-        leading: const BackButton(
-          color: spqBlack,
-        ),
-        backgroundColor: spqWhite,
-      ),
-      body: Container(
-        color: spqWhite,
-        child: Center(
-          child: Hero(
-            tag: 'myImage',
-            child: Image.network(
-                "https://unicheck.unicum.de/sites/default/files/artikel/image/informatik-kannst-du-auch-auf-englisch-studieren-gettyimages-rosshelen-uebersichtsbild.jpg"),
-          ),
-        ),
-      ),
     );
   }
 }
