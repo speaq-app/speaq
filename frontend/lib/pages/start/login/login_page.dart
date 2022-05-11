05776bcf080e9ec16d89b4842523b49260cb5fb6
eeisemann220 <85163516+eeisemann220@users.noreply.github.com>
2022-05-11 08:28:03 +0200

    feat: SWPG6SPEAK-202/203-new-register-page (#43)
    * feat: add new register_page
    
    * refactor: extract widgets, cleanup code
    
    * refactor: refactor/clean up code
    
    * refactor: refactor/clean up code
    feat: add approve methods
    
    * feat: add internationalization, add functions
    refactor: refactor/clean up code
    
    * refactor:
    review problems fixed
    
    * fix:
    delete accountCheck, refactor code
    
    * fix:
    add password approved, refactor code

--- frontend/lib/pages/start/login/login_page.dart
+++ frontend/lib/pages/start/login/login_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:frontend/utils/all_utils.dart';
-import 'package:frontend/utils/speaq_styles.dart';
 import 'package:frontend/widgets/all_widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,158 +11,118 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final String langKey = "pages.start.login.";
-
-  final String name = "Username";
-  final String password = "password";
-  final String forgot = "Forgot Password?";
-  final String loginText = "Login";
-  final String registerText = "You don't have an Account?";
-  final String register = "Register";
-  final String hinweisLogin = "Du hast noch keinen Account?";
-  final String hinweisRegister = "Besitzt du bereits einen Account?";
-  final String homeText = "Du möchtest als Gast beitreten?";
-  final String home = "Gast";
   bool isHidden = true;
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) => Drawer(
-        child: SafeArea(
-          child: ListView(
-            children: <Widget>[
-              buildTop(context),
-              buildBottom(context),
-            ],
-          ),
-        ),
-      );
+  Widget build(BuildContext context) {
+    AppLocalizations appLocale = AppLocalizations.of(context)!;
 
-  Widget buildTop(BuildContext context) => Container(
-        padding: const EdgeInsets.only(
-          top: 125,
-          bottom: 50,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Drawer(
+      child: SafeArea(
+        child: ListView(
           children: <Widget>[
-            SvgPicture.asset(
-              "assets/images/logo/speaq_text_logo.svg",
-              height: 75,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 80),
-              child: RoundInputField(
-                hintText: name,
-                onChanged: (value) {},
-                controller: _nameController,
-              ),
-            ),
-            RoundPasswordField(
-              password: password,
-              isHidden: isHidden,
-              controller: _passwordController,
-              suffixIcon: InkWell(
-                onTap: () {
-                  setState(() {
-                    isHidden = !isHidden;
-                  });
-                },
-                child: isHidden
-                    ? const Icon(
-                        Icons.visibility_off,
-                        color: Colors.black,
-                      )
-                    : const Icon(
-                        Icons.visibility,
-                        color: Colors.black,
-                      ),
-              ),
-              onChanged: (String value) {},
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Text(
-                forgot,
-                textAlign: TextAlign.right,
-                style: const TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+            buildTop(context, appLocale),
+            buildBottom(context, appLocale),
           ],
         ),
-      );
+      ),
+    );
+  }
 
-  Widget buildBottom(BuildContext context) => Column(
+  Widget buildTop(BuildContext context, AppLocalizations appLocale) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 125,
+        bottom: 50,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          SvgPicture.asset(
+            "assets/images/logo/speaq_text_logo.svg",
+            height: 75,
+          ),
           Padding(
-            padding: const EdgeInsets.only(
-              bottom: 30,
-              top: 10,
-            ),
-            child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: spqPrimaryBlue,
-                  padding: const EdgeInsets.all(15.0),
-                  fixedSize: const Size(200, 50),
-                  primary: Colors.white,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(29),
-                    ),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.popAndPushNamed(context, "base");
-                },
-                child: Text(loginText)),
-          ),
-          AccountCheck(
-            hinweisLogin: hinweisLogin,
-            hinweisRegister: hinweisRegister,
-            register: register,
-            loginText: loginText,
-            press: () {
-              Navigator.popAndPushNamed(context, "register");
-            },
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 5, bottom: 5),
-            child: Divider(
-              color: Colors.black54,
-              thickness: 0.75,
+            padding: const EdgeInsets.only(top: 80),
+            child: RoundTextField(
+              autofill: const [AutofillHints.name],
+              icon: Icons.person,
+              hintText: appLocale.username,
+              onChanged: (value) {},
+              controller: _nameController,
+              labelTex: appLocale.username,
+              borderColor: Border.all(color: Colors.black26),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(right: 5),
-                child: Text(
-                  homeText,
-                  style: const TextStyle(fontSize: 10),
-                ),
+          RoundTextField(
+            autofill: const [AutofillHints.password],
+            icon: Icons.lock,
+            hintText: appLocale.password,
+            isHidden: isHidden,
+            labelTex: appLocale.password,
+            controller: _passwordController,
+            suffixIcon: _buildVisibility(),
+            onChanged: (String value) {},
+            borderColor: Border.all(color: Colors.black26),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Text(
+              appLocale.passwordForgot,
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.popAndPushNamed(context, "base");
-                },
-                child: Text(
-                  home,
-                  style: const TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ],
-      );
+      ),
+    );
+  }
+
+  Widget buildBottom(BuildContext context, AppLocalizations appLocale) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 30,
+            top: 10,
+          ),
+          child: SpeaqButton(
+            loginText: appLocale.login,
+            onPressed: () {
+              Navigator.popAndPushNamed(context, "base");
+            },
+          ),
+        ),
+        SpeaqPageForwarding(
+          hintText: appLocale.loginHint,
+          text: appLocale.register,
+          press: () {
+            Navigator.popAndPushNamed(context, "register");
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Divider(
+            color: Colors.black54,
+            thickness: 0.75,
+          ),
+        ),
+        SpeaqPageForwarding(
+          hintText: appLocale.guestText,
+          text: appLocale.guest,
+          press: () {
+            Navigator.popAndPushNamed(context, "base");
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   void dispose() {
@@ -174,5 +133,26 @@ class _LoginPageState extends State<LoginPage> {
   void _disposeController() {
     _passwordController.dispose();
     _nameController.dispose();
+  }
+
+  InkWell _buildVisibility() {
+    return InkWell(
+      onTap: () {
+        setState(
+          () {
+            isHidden = !isHidden;
+          },
+        );
+      },
+      child: isHidden
+          ? const Icon(
+              Icons.visibility,
+              color: Colors.black,
+            )
+          : const Icon(
+              Icons.visibility_off,
+              color: Colors.black,
+            ),
+    );
   }
 }
---
+++ frontend/lib/widgets/speaq_login_register_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/utils/speaq_styles.dart';
+
+class SpeaqButton extends StatelessWidget {
+  const SpeaqButton({
+    Key? key,
+    required this.loginText,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String loginText;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          backgroundColor: spqPrimaryBlue,
+          padding: const EdgeInsets.all(15.0),
+          fixedSize: const Size(200, 50),
+          primary: Colors.white,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(29),
+            ),
+          ),
+        ),
+        onPressed: () {
+          Navigator.popAndPushNamed(context, "base");
+        },
+        child: Text(loginText));
+  }
+}
+
+class SpeaqPageForwarding extends StatelessWidget {
+  final Function press;
+
+  const SpeaqPageForwarding({
+    Key? key,
+    required this.press,
+    required this.hintText,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 5),
+          child: Text(
+            hintText,
+            style: const TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () => press(),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 10,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
