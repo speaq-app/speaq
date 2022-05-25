fb30b8026182b83b7b068e1bc7207e024b131e47
Sven Gatnar <sgatnar@stud.hs-heilbronn.de>
2022-05-25 12:14:02 +0200

    search functionality

--- frontend/lib/pages/base/search/search_page.dart
+++ frontend/lib/pages/base/search/search_page.dart
@@ -11,15 +11,61 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<User> _allUserList = [
+    User(
+        "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+        "Sven Gatnar",
+        "Ich bin so ein geiler Typ",
+        "12:20"),
+    User(
+        "https://hotdogworld.de/media/image/dd/5f/5f/HZ_TK_500ml_USD_1280x1280.jpg",
+        "Halois Ainz",
+        "Ich kenne jeden Automaten den es gibt",
+        "12:33"),
+    User(
+        "http://staffmobility.eu/sites/default/files/isewtweetbg.jpg",
+        "Hendrik Schlehlein",
+        "Daily-Meetings verlaufen nicht so gut",
+        "01.01.2020"),
+    User(
+        "https://upload.wikimedia.org/wikipedia/commons/c/c9/Moon.jpg",
+        "Nosakhare Omoruyi",
+        "Ich bin der allerbeste Programmiererer",
+        "20.04.2020"),
+    User("https://img1.dreamies.de/img/21/b/p4el6oypvd7.jpg", "David Löwe",
+        "Elvis ist mein Ein und Alles", "03.01.2020"),
+    User("https://www.meinkleinesparadies.de/images/52/07.02.16-026.JPG",
+        "Eric Eisemann", "Ich hätte gerne einen Bart und eine Glatze", "15:30"),
+    User(
+        "https://www.rollingstone.co.uk/wp-content/uploads/sites/2/2021/11/lil-nas-x-press-1024x650.jpeg",
+        "Daniel Holzfuß",
+        "Nicht nur mein Fuß ist aus Holz",
+        "11:11"),
+    User("https://img1.dreamies.de/img/21/b/p4el6oypvd7.jpg", "NoSack Haare",
+        "Open to work", "12:59"),
+    User(
+        "https://img1.dreamies.de/img/21/b/p4el6oypvd7.jpg",
+        "Hendrik HaveAChin",
+        "Ich bin so geil, ich könnte den Ball ...",
+        "18:20"),
+    User("https://img1.dreamies.de/img/21/b/p4el6oypvd7.jpg", "Dani Holzwarth",
+        "Am Meisten mag ich an mir meine roten Haare", "01:10"),
+  ];
+  List<User> _foundUsersList = [];
+
+  @override
+  void initState() {
+    _foundUsersList = _allUserList;
+    super.initState();
+  }
 
   late AppLocalizations appLocale;
   late Size deviceSize;
 
   @override
   Widget build(BuildContext context) {
-    deviceSize = MediaQuery.of(context).size;
     appLocale = AppLocalizations.of(context)!;
-
+    deviceSize = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: SpqAppBar(
@@ -32,6 +78,29 @@ class _SearchPageState extends State<SearchPage> {
           preferredSize: Size.fromHeight(1600),
           centerTitle: true,
         ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _foundUsersList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(_foundUsersList[index].profilePic),
+                    ),
+                    title: Text(
+                      _foundUsersList[index].name,
+                    ),
+                    subtitle: Text(_foundUsersList[index].lastMessage),
+                    trailing: Text(_foundUsersList[index].time),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -43,7 +112,7 @@ class _SearchPageState extends State<SearchPage> {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
         child: TextField(
-          //onChanged: (value) => filterSearchResults(value),
+          onChanged: (value) => filterSearchResults(value),
           decoration: InputDecoration(
             filled: true,
             prefixIcon: const Icon(Icons.search, color: spqDarkGreyTranslucent),
@@ -54,10 +123,26 @@ class _SearchPageState extends State<SearchPage> {
               fontSize: 14,
               color: spqDarkGreyTranslucent,
             ),
-            hintText: appLocale.hintTextSearchBar,
+            hintText: appLocale.hintTextSearchBarSearchPage,
           ),
         ),
       ),
     );
+  }
+
+  filterSearchResults(String text) {
+    List<User> filterList = <User>[];
+    if (text != null) {
+      setState(
+        () {
+          filterList.addAll(_allUserList
+              .where((user) => user.name.toString().contains(text))
+              .toList());
+          _foundUsersList = filterList;
+        },
+      );
+    } else {
+      _foundUsersList = _allUserList;
+    }
   }
 }
