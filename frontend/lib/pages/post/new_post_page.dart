fb8281a8c58ee7a706577a65af344e79a3619a5d
Sven Gatnar <sgatnar@stud.hs-heilbronn.de>
2022-05-27 17:56:57 +0200

    camera and gallerly

--- frontend/lib/pages/post/new_post_page.dart
+++ frontend/lib/pages/post/new_post_page.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,6 +6,7 @@ import 'package:frontend/utils/all_utils.dart';
 import 'package:frontend/widgets/all_widgets.dart';
 import 'package:frontend/widgets/speaq_post_text_field.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:image_picker/image_picker.dart';
 
 class NewPostPage extends StatefulWidget {
   const NewPostPage({Key? key}) : super(key: key);
@@ -19,11 +19,29 @@ class _NewPostPageState extends State<NewPostPage> {
   late Size deviceSize;
   late AppLocalizations appLocale;
   final TextEditingController _postController = TextEditingController();
-  final TextEditingController _controllerEmoji = TextEditingController();
   bool emojiShowing = false;
   bool keyboardShowing = true;
   bool visibilityContainer = false;
   bool checkValue = false;
+  bool checkImage = false;
+  File? _image = new File("assets/images/developer_sven.jpg");
+  late XFile? im;
+
+  bool isAudioClicked = false;
+
+  Future getImage(bool isCamera) async {
+    if (isCamera) {
+      im = await ImagePicker().pickImage(source: ImageSource.camera);
+    } else {
+      im = await ImagePicker().pickImage(source: ImageSource.gallery);
+    }
+
+    setState(
+      () {
+        _image = File(im!.path);
+      },
+    );
+  }
 
   @override
   void initState() {
@@ -42,6 +60,11 @@ class _NewPostPageState extends State<NewPostPage> {
 
         if (keyboardShowing && emojiShowing) {
           emojiShowing = false;
+        } else if (keyboardShowing && isAudioClicked) {
+          isAudioClicked = false;
+        } else if (emojiShowing && isAudioClicked) {
+          isAudioClicked = false;
+
         }
       },
     );
@@ -67,58 +90,69 @@ class _NewPostPageState extends State<NewPostPage> {
     appLocale = AppLocalizations.of(context)!;
     return SafeArea(
       child: Scaffold(
-          appBar: SpqAppBar(
-            preferredSize: deviceSize,
-            actionList: [_buildSendPostButton(appLocale)],
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                child: _buildPostTextField(appLocale),
+        appBar: SpqAppBar(
+          preferredSize: deviceSize,
+          actionList: [_buildSendPostButton(appLocale)],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: _buildPostTextField(appLocale),
+            ),
+            buildVisibilityContainer(),
+            buildMainContainer(deviceSize),
+            Offstage(
+              offstage: !emojiShowing,
+              child: SizedBox(
+                height: 280,
+                child: EmojiPicker(
+                    onEmojiSelected: (Category category, Emoji emoji) {
+                      onEmojiSelected(emoji);
+                    },
+                    onBackspacePressed: _onBackspacePressed,
+                    config: Config(
+                        columns: 7,
+                        emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                        verticalSpacing: 0,
+                        horizontalSpacing: 0,
+                        initCategory: Category.RECENT,
+                        bgColor: const Color(0xFFF2F2F2),
+                        indicatorColor: Colors.blue,
+                        iconColor: Colors.grey,
+                        iconColorSelected: Colors.blue,
+                        progressIndicatorColor: Colors.blue,
+                        backspaceColor: Colors.blue,
+                        skinToneDialogBgColor: Colors.white,
+                        skinToneIndicatorColor: Colors.grey,
+                        enableSkinTones: true,
+                        showRecentsTab: true,
+                        recentsLimit: 28,
+                        noRecents: Text(
+                          appLocale.noRecents,
+                          style: TextStyle(fontSize: 20, color: Colors.black26),
+                          textAlign: TextAlign.center,
+                        ),
+                        tabIndicatorAnimDuration: kTabScrollDuration,
+                        categoryIcons: const CategoryIcons(),
+                        buttonMode: ButtonMode.MATERIAL)),
               ),
-              buildVisibilityContainer(),
-              buildMainContainer(deviceSize),
-              Offstage(
-                offstage: !emojiShowing,
-                child: SizedBox(
-                  height: 280,
-                  child: EmojiPicker(
-                      onEmojiSelected: (Category category, Emoji emoji) {
-                        onEmojiSelected(emoji);
-                      },
-                      onBackspacePressed: _onBackspacePressed,
-                      config: Config(
-                          columns: 7,
-                          emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
-                          verticalSpacing: 0,
-                          horizontalSpacing: 0,
-                          initCategory: Category.RECENT,
-                          bgColor: const Color(0xFFF2F2F2),
-                          indicatorColor: Colors.blue,
-                          iconColor: Colors.grey,
-                          iconColorSelected: Colors.blue,
-                          progressIndicatorColor: Colors.blue,
-                          backspaceColor: Colors.blue,
-                          skinToneDialogBgColor: Colors.white,
-                          skinToneIndicatorColor: Colors.grey,
-                          enableSkinTones: true,
-                          showRecentsTab: true,
-                          recentsLimit: 28,
-                          noRecents: const Text(
-                            'No Recents',
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.black26),
-                            textAlign: TextAlign.center,
-                          ),
-                          tabIndicatorAnimDuration: kTabScrollDuration,
-                          categoryIcons: const CategoryIcons(),
-                          buttonMode: ButtonMode.MATERIAL)),
+            ),
+            Offstage(
+              offstage: !isAudioClicked,
+              child: SizedBox(
+                height: 280,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.red,
+                    onPressed: () {},
+                  ),
                 ),
               ),
-            ],
-          )
-          //_buildPostTextField(appLocale),
-          ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -131,7 +165,7 @@ class _NewPostPageState extends State<NewPostPage> {
           scrollDirection: Axis.horizontal,
           child: Row(
             children: [
-              buildContainerPictures(
+              buildContainerCamera(
                   'https://i.pinimg.com/736x/61/54/18/61541805b3069740ecd60d483741e5bb.jpg'),
               buildContainerPictures(
                   'https://9to5fortnite.com/de/wp-content/uploads/2022/04/Corinna-Kopf-Twitch-Zuschauerzahlen-boomen-nach-dem-Wechsel-zu-IRL-Streams.jpg'),
@@ -156,7 +190,14 @@ class _NewPostPageState extends State<NewPostPage> {
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: () {
-          print('Hello World');
+          getImage(false);
+          setState(() {
+            if (checkImage) {
+              checkImage = !checkImage;
+            } else {
+              checkImage = true;
+            }
+          });
         },
         child: Container(
           decoration: BoxDecoration(
@@ -198,7 +239,6 @@ class _NewPostPageState extends State<NewPostPage> {
               child: TextFormField(
                 autofocus: true,
                 controller: _postController,
-                minLines: 1,
                 style: const TextStyle(fontSize: 20.0, color: Colors.black87),
                 decoration: InputDecoration(
                   suffixIcon: Padding(
@@ -233,14 +273,25 @@ class _NewPostPageState extends State<NewPostPage> {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: IconButton(
-              icon: new Icon(Icons.multitrack_audio),
-              onPressed: () {},
-            ),
-          ),
+          buildAudio(),
         ],
+      ),
+    );
+  }
+
+  Padding buildAudio() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: IconButton(
+        icon: new Icon(Icons.multitrack_audio),
+        onPressed: () {
+          if (keyboardShowing) {
+            SystemChannels.textInput.invokeMethod('TextInput.hide');
+          }
+          setState(() {
+            isAudioClicked = !isAudioClicked;
+          });
+        },
       ),
     );
   }
@@ -248,41 +299,54 @@ class _NewPostPageState extends State<NewPostPage> {
   Widget _buildPostTextField(AppLocalizations appLocale) => Padding(
         padding: const EdgeInsets.all(8.0),
         child: SpqPostTextField(
+          suffixIcon: Visibility(
+            visible: checkImage,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Container(
+                  child: Image.file(_image!),
+                ),
+              ),
+            ),
+          ),
           height: double.infinity,
-          minLines: 30,
+          maxLines: 30,
           controller: _postController,
           hintText: appLocale.newPost,
         ),
       );
 
-  Widget _buildSendPostButton(AppLocalizations appLocale) => TextButton(
-        onPressed: () => showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            Future.delayed(
-              Duration(seconds: 1),
-              () {
-                Navigator.popAndPushNamed(context, "home");
-              },
-            );
-            return AlertDialog(
-              title: Text(appLocale.createdPost),
-              backgroundColor: spqLightGrey,
-            );
-          },
-        ),
-        child: Container(
-          child: const Text("speaq"),
-          margin: const EdgeInsets.symmetric(horizontal: 4.0),
-          padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),
-          decoration: BoxDecoration(
-            border: Border.all(color: spqPrimaryBlue, width: 1.0),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(16.0),
-            ),
+  Widget _buildSendPostButton(AppLocalizations appLocale) {
+    return TextButton(
+      onPressed: () => showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          Future.delayed(
+            Duration(seconds: 1),
+            () {
+              Navigator.popAndPushNamed(context, "home");
+            },
+          );
+          return AlertDialog(
+            title: Text(appLocale.createdPost),
+            backgroundColor: spqLightGrey,
+          );
+        },
+      ),
+      child: Container(
+        child: const Text("Speaq"),
+        margin: const EdgeInsets.symmetric(horizontal: 4.0),
+        padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: spqPrimaryBlue, width: 1.0),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(16.0),
           ),
         ),
-      );
+      ),
+    );
+  }
 
   void emojiClick() {
     if (keyboardShowing) {
@@ -292,6 +356,28 @@ class _NewPostPageState extends State<NewPostPage> {
       () {
         emojiShowing = !emojiShowing;
       },
+    );
+  }
+
+  buildContainerCamera(String url) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          getImage(true);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.black12, borderRadius: BorderRadius.circular(5)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: SizedBox.fromSize(
+              size: Size.fromRadius(32), // Image radius
+              child: Image.network(url, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
