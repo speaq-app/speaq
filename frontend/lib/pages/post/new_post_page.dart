30a6fe9b3d062c3e584fee66efb753fc3f9ce96a
Sven Gatnar <sgatnar@stud.hs-heilbronn.de>
2022-05-30 23:44:09 +0200

    audio function

--- frontend/lib/pages/post/new_post_page.dart
+++ frontend/lib/pages/post/new_post_page.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:frontend/utils/all_utils.dart';
 import 'package:frontend/widgets/all_widgets.dart';
 import 'package:frontend/widgets/speaq_post_text_field.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class NewPostPage extends StatefulWidget {
   const NewPostPage({Key? key}) : super(key: key);
@@ -16,72 +19,45 @@ class NewPostPage extends StatefulWidget {
 }
 
 class _NewPostPageState extends State<NewPostPage> {
+  // Main
   late Size deviceSize;
   late AppLocalizations appLocale;
   final TextEditingController _postController = TextEditingController();
-  bool emojiShowing = false;
-  bool keyboardShowing = true;
+
+  // Keyboard
+  bool keyboardMainVisible = true;
   bool visibilityContainer = false;
   bool checkValue = false;
+
+  // Emoji
+  bool emojiKeyboardVisible = false;
+
+  // Camera/Gallery
   bool checkImage = false;
   File? _image = new File("assets/images/developer_sven.jpg");
   late XFile? im;
 
-  bool isAudioClicked = false;
-
-  Future getImage(bool isCamera) async {
-    if (isCamera) {
-      im = await ImagePicker().pickImage(source: ImageSource.camera);
-    } else {
-      im = await ImagePicker().pickImage(source: ImageSource.gallery);
-    }
-
-    setState(
-      () {
-        _image = File(im!.path);
-      },
-    );
-  }
+  // Audio
+  final recorder = FlutterSoundRecorder();
+  bool audioKeyboardVisible = false;
+  bool isRecorderReady = false;
+  String recordingText = "00:00";
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-      visibilityContainer = checkValue;
-    });
 
-    KeyboardVisibility.onChange.listen(
-      (bool keyboardShowing) {
-        setState(
-          () {
-            this.keyboardShowing = keyboardShowing;
-          },
-        );
+    // Initialize Recorder for Audio
+    initRecorder();
 
-        if (keyboardShowing && emojiShowing) {
-          emojiShowing = false;
-        } else if (keyboardShowing && isAudioClicked) {
-          isAudioClicked = false;
-        } else if (emojiShowing && isAudioClicked) {
-          isAudioClicked = false;
+    // KEYBOARD VISIBILITY
+    keyboardInput();
 
-        }
+    setState(
+      () {
+        visibilityContainer = checkValue;
       },
     );
-  }
-
-  onEmojiSelected(Emoji emoji) {
-    _postController
-      ..text += emoji.emoji
-      ..selection = TextSelection.fromPosition(
-          TextPosition(offset: _postController.text.length));
-  }
-
-  _onBackspacePressed() {
-    _postController
-      ..text = _postController.text.characters.skipLast(1).toString()
-      ..selection = TextSelection.fromPosition(
-          TextPosition(offset: _postController.text.length));
   }
 
   @override
@@ -101,130 +77,35 @@ class _NewPostPageState extends State<NewPostPage> {
             ),
             buildVisibilityContainer(),
             buildMainContainer(deviceSize),
-            Offstage(
-              offstage: !emojiShowing,
-              child: SizedBox(
-                height: 280,
-                child: EmojiPicker(
-                    onEmojiSelected: (Category category, Emoji emoji) {
-                      onEmojiSelected(emoji);
-                    },
-                    onBackspacePressed: _onBackspacePressed,
-                    config: Config(
-                        columns: 7,
-                        emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
-                        verticalSpacing: 0,
-                        horizontalSpacing: 0,
-                        initCategory: Category.RECENT,
-                        bgColor: const Color(0xFFF2F2F2),
-                        indicatorColor: Colors.blue,
-                        iconColor: Colors.grey,
-                        iconColorSelected: Colors.blue,
-                        progressIndicatorColor: Colors.blue,
-                        backspaceColor: Colors.blue,
-                        skinToneDialogBgColor: Colors.white,
-                        skinToneIndicatorColor: Colors.grey,
-                        enableSkinTones: true,
-                        showRecentsTab: true,
-                        recentsLimit: 28,
-                        noRecents: Text(
-                          appLocale.noRecents,
-                          style: TextStyle(fontSize: 20, color: Colors.black26),
-                          textAlign: TextAlign.center,
-                        ),
-                        tabIndicatorAnimDuration: kTabScrollDuration,
-                        categoryIcons: const CategoryIcons(),
-                        buttonMode: ButtonMode.MATERIAL)),
-              ),
-            ),
-            Offstage(
-              offstage: !isAudioClicked,
-              child: SizedBox(
-                height: 280,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: FloatingActionButton(
-                    backgroundColor: Colors.red,
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-            ),
+            buildOffstageEmoji(),
+            buildOffstageAudio(),
           ],
         ),
       ),
     );
   }
 
-  Visibility buildVisibilityContainer() {
-    return Visibility(
-      child: Container(
-        height: deviceSize.height * 0.1,
-        width: deviceSize.width,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              buildContainerCamera(
-                  'https://i.pinimg.com/736x/61/54/18/61541805b3069740ecd60d483741e5bb.jpg'),
-              buildContainerPictures(
-                  'https://9to5fortnite.com/de/wp-content/uploads/2022/04/Corinna-Kopf-Twitch-Zuschauerzahlen-boomen-nach-dem-Wechsel-zu-IRL-Streams.jpg'),
-              buildContainerPictures(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7k50ZmoaI9mkcYWJArxPWdkpSNf7QM8UzOd43LIj69CP2XzLkq9tD-4uz4s_Al9EJfK4&usqp=CAU'),
-              buildContainerPictures(
-                  'https://media-exp1.licdn.com/dms/image/D4E35AQFdsSxUaOeh7A/profile-framedphoto-shrink_800_800/0/1626087039018?e=1653922800&v=beta&t=TDYjF0_qwmfYWPVN0YqoSyV4VX85zEDNMZ4OuaruY8A'),
-              buildContainerPictures(
-                  'https://media-exp1.licdn.com/dms/image/D4E35AQHsZeU9aUaDcA/profile-framedphoto-shrink_800_800/0/1651314690282?e=1653922800&v=beta&t=Wwe_zVZTeMQr4iizRPy8aOqc4tfGlzeIH8WuhJngTAI'),
-              buildContainerPictures(
-                  'https://media-exp1.licdn.com/dms/image/C4D03AQFFnndLd3cUog/profile-displayphoto-shrink_800_800/0/1649862939707?e=1658966400&v=beta&t=UzNwV5wS111quhYnnInTrSNO1mHknooTLsO_iceQ0d0'),
-            ],
-          ),
-        ),
-      ),
-      visible: checkValue,
+  void keyboardInput() {
+    KeyboardVisibility.onChange.listen(
+      (bool keyboardShowing) {
+        setState(
+          () {
+            this.keyboardMainVisible = keyboardShowing;
+          },
+        );
+
+        if (keyboardShowing && emojiKeyboardVisible) {
+          emojiKeyboardVisible = false;
+        } else if (keyboardShowing && audioKeyboardVisible) {
+          audioKeyboardVisible = false;
+        } else if (emojiKeyboardVisible && audioKeyboardVisible) {
+          audioKeyboardVisible = false;
+        }
+      },
     );
   }
 
-  Padding buildContainerPictures(String url) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () {
-          getImage(false);
-          setState(() {
-            if (checkImage) {
-              checkImage = !checkImage;
-            } else {
-              checkImage = true;
-            }
-          });
-        },
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.black12, borderRadius: BorderRadius.circular(5)),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(5),
-            child: SizedBox.fromSize(
-              size: Size.fromRadius(32), // Image radius
-              child: Image.network(url, fit: BoxFit.cover),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Material buildMaterial(IconData icon, void Function()? onPressed) {
-    return Material(
-      child: IconButton(
-        onPressed: onPressed,
-        icon: Icon(
-          icon,
-          color: Colors.black54,
-        ),
-      ),
-    );
-  }
+  /// MAIN CONTAINER
 
   Container buildMainContainer(Size deviceSize) {
     return Container(
@@ -279,17 +160,203 @@ class _NewPostPageState extends State<NewPostPage> {
     );
   }
 
+  Visibility buildVisibilityContainer() {
+    return Visibility(
+      child: Container(
+        height: deviceSize.height * 0.1,
+        width: deviceSize.width,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              buildContainerCamera(
+                  'https://i.pinimg.com/736x/61/54/18/61541805b3069740ecd60d483741e5bb.jpg'),
+              buildContainerPictures(
+                  'https://9to5fortnite.com/de/wp-content/uploads/2022/04/Corinna-Kopf-Twitch-Zuschauerzahlen-boomen-nach-dem-Wechsel-zu-IRL-Streams.jpg'),
+              buildContainerPictures(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7k50ZmoaI9mkcYWJArxPWdkpSNf7QM8UzOd43LIj69CP2XzLkq9tD-4uz4s_Al9EJfK4&usqp=CAU'),
+              buildContainerPictures(
+                  'https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557376304.186_U5U7u5_n.jpg'),
+              buildContainerPictures(
+                  'https://media-exp1.licdn.com/dms/image/C4D03AQFFnndLd3cUog/profile-displayphoto-shrink_800_800/0/1649862939707?e=1658966400&v=beta&t=UzNwV5wS111quhYnnInTrSNO1mHknooTLsO_iceQ0d0'),
+              buildContainerPictures(
+                  'https://media-exp1.licdn.com/dms/image/C4D03AQFFnndLd3cUog/profile-displayphoto-shrink_800_800/0/1649862939707?e=1658966400&v=beta&t=UzNwV5wS111quhYnnInTrSNO1mHknooTLsO_iceQ0d0'),
+            ],
+          ),
+        ),
+      ),
+      visible: checkValue,
+    );
+  }
+
+  Material buildMaterial(IconData icon, void Function()? onPressed) {
+    return Material(
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          color: spqBlack,
+        ),
+      ),
+    );
+  }
+
+  /// EMOJI
+
+  Offstage buildOffstageEmoji() {
+    return Offstage(
+      offstage: !emojiKeyboardVisible,
+      child: SizedBox(
+        height: 280,
+        child: EmojiPicker(
+            onEmojiSelected: (Category category, Emoji emoji) {
+              onEmojiSelected(emoji);
+            },
+            onBackspacePressed: _onBackspacePressed,
+            config: Config(
+                columns: 7,
+                emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                verticalSpacing: 0,
+                horizontalSpacing: 0,
+                initCategory: Category.RECENT,
+                bgColor: const Color(0xFFF2F2F2),
+                indicatorColor: Colors.blue,
+                iconColor: Colors.grey,
+                iconColorSelected: Colors.blue,
+                progressIndicatorColor: Colors.blue,
+                backspaceColor: Colors.blue,
+                skinToneDialogBgColor: Colors.white,
+                skinToneIndicatorColor: Colors.grey,
+                enableSkinTones: true,
+                showRecentsTab: true,
+                recentsLimit: 28,
+                noRecents: Text(
+                  appLocale.noRecents,
+                  style: TextStyle(fontSize: 20, color: Colors.black26),
+                  textAlign: TextAlign.center,
+                ),
+                tabIndicatorAnimDuration: kTabScrollDuration,
+                categoryIcons: const CategoryIcons(),
+                buttonMode: ButtonMode.MATERIAL)),
+      ),
+    );
+  }
+
+  onEmojiSelected(Emoji emoji) {
+    _postController
+      ..text += emoji.emoji
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _postController.text.length));
+  }
+
+  _onBackspacePressed() {
+    setState(() {
+      if (emojiKeyboardVisible) {
+        emojiKeyboardVisible = !emojiKeyboardVisible;
+        audioKeyboardVisible = true;
+      } else {
+        emojiKeyboardVisible = true;
+        audioKeyboardVisible = false;
+      }
+      ;
+    });
+    _postController
+      ..text = _postController.text.characters.skipLast(1).toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _postController.text.length));
+  }
+
+  void emojiClick() {
+    if (keyboardMainVisible) {
+      SystemChannels.textInput.invokeMethod('TextInput.hide');
+    }
+    setState(
+      () {
+        emojiKeyboardVisible = !emojiKeyboardVisible;
+      },
+    );
+  }
+
+  /// BUILD CAMERA/GALLERY AND FUNCTIONALITIES
+
+  Padding buildContainerPictures(String url) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          getImage(false);
+          setState(() {
+            if (checkImage) {
+              checkImage = !checkImage;
+            } else {
+              checkImage = true;
+            }
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: spqBlack, borderRadius: BorderRadius.circular(5)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: SizedBox.fromSize(
+              size: Size.fromRadius(32), // Image radius
+              child: Image.network(url, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future getImage(bool isCamera) async {
+    if (isCamera) {
+      im = await ImagePicker().pickImage(source: ImageSource.camera);
+    } else {
+      im = await ImagePicker().pickImage(source: ImageSource.gallery);
+    }
+
+    setState(
+      () {
+        _image = File(im!.path);
+      },
+    );
+  }
+
+  buildContainerCamera(String url) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          getImage(true);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.black12, borderRadius: BorderRadius.circular(5)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: SizedBox.fromSize(
+              size: Size.fromRadius(32), // Image radius
+              child: Image.network(url, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// BUILD ALL BUTTONS IN KEYBOARD
+
   Padding buildAudio() {
     return Padding(
       padding: EdgeInsets.all(8.0),
       child: IconButton(
         icon: new Icon(Icons.multitrack_audio),
         onPressed: () {
-          if (keyboardShowing) {
+          if (keyboardMainVisible) {
             SystemChannels.textInput.invokeMethod('TextInput.hide');
           }
           setState(() {
-            isAudioClicked = !isAudioClicked;
+            audioKeyboardVisible = !audioKeyboardVisible;
           });
         },
       ),
@@ -304,9 +371,7 @@ class _NewPostPageState extends State<NewPostPage> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                child: Container(
-                  child: Image.file(_image!),
-                ),
+                child: Image.file(_image!),
               ),
             ),
           ),
@@ -348,36 +413,109 @@ class _NewPostPageState extends State<NewPostPage> {
     );
   }
 
-  void emojiClick() {
-    if (keyboardShowing) {
-      SystemChannels.textInput.invokeMethod('TextInput.hide');
-    }
-    setState(
-      () {
-        emojiShowing = !emojiShowing;
-      },
-    );
-  }
+  /// AUDIO
 
-  buildContainerCamera(String url) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+  Offstage buildOffstageAudio() {
+    return Offstage(
+      offstage: !audioKeyboardVisible,
       child: InkWell(
         onTap: () {
-          getImage(true);
+          setState(() {
+            if (audioKeyboardVisible) {
+              audioKeyboardVisible = !audioKeyboardVisible;
+            } else {
+              audioKeyboardVisible = true;
+            }
+          });
         },
         child: Container(
-          decoration: BoxDecoration(
-              color: Colors.black12, borderRadius: BorderRadius.circular(5)),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(5),
-            child: SizedBox.fromSize(
-              size: Size.fromRadius(32), // Image radius
-              child: Image.network(url, fit: BoxFit.cover),
+          height: 280,
+          width: 80,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildAudioButtonFunction(),
+                StreamBuilder<RecordingDisposition>(
+                  stream: recorder.onProgress,
+                  builder: (context, snapshot) {
+                    final duration = snapshot.hasData
+                        ? snapshot.data!.duration
+                        : Duration.zero;
+
+                    String twoDigits(int n) => n.toString().padLeft(2, "0");
+                    String twoDigitMinutes =
+                        twoDigits(duration.inMinutes.remainder(60));
+                    String twoDigitSeconds =
+                        twoDigits(duration.inSeconds.remainder(60));
+
+                    return Text(
+                      '$twoDigitMinutes:$twoDigitSeconds',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildAudioButtonFunction() {
+    return ElevatedButton(
+      child: Icon(recorder.isRecording ? Icons.stop : Icons.mic, size: 32),
+      style: ElevatedButton.styleFrom(
+          fixedSize: Size(132, 132), shape: CircleBorder()),
+      onPressed: () async {
+        if (recorder.isRecording) {
+          await stop();
+        } else {
+          await record();
+        }
+        setState(
+          () {},
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    recorder.closeRecorder();
+
+    super.dispose();
+  }
+
+  Future record() async {
+    if (!isRecorderReady) return;
+
+    await recorder.startRecorder(toFile: 'audio');
+  }
+
+  Future stop() async {
+    if (!isRecorderReady) return;
+
+    final path = await recorder.stopRecorder();
+    final audiopath = File(path!);
+
+    print('Recorded audio: $audiopath');
+  }
+
+  Future initRecorder() async {
+    final status = await Permission.microphone.request();
+
+    if (status != PermissionStatus.granted) {
+      throw 'Microphone permission not granted';
+    }
+
+    await recorder.openRecorder();
+    isRecorderReady = true;
+
+    recorder.setSubscriptionDuration(
+      const Duration(milliseconds: 500),
     );
   }
 }
