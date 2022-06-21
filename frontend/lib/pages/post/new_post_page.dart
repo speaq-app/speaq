5ee8816455a5195b10712e2e18ade8b87d3607b3
nomoruyi <nomoruyi@stud.hs-heilbronn.de>
2022-06-21 13:31:55 +0200

    feat: keyboard in bloc pattern

--- frontend/lib/pages/post/new_post_page.dart
+++ frontend/lib/pages/post/new_post_page.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 import 'dart:io';
+
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/api/model/post.dart';
 import 'package:frontend/blocs/post_bloc/post_bloc.dart';
 import 'package:frontend/blocs/resource_bloc/resource_bloc.dart';
@@ -16,8 +17,8 @@ import 'package:frontend/widgets/all_widgets.dart';
 import 'package:frontend/widgets/speaq_post_text_field.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class NewPostPage extends StatefulWidget {
   const NewPostPage({Key? key, required this.userID}) : super(key: key);
@@ -107,7 +108,6 @@ class _NewPostPageState extends State<NewPostPage> {
         break;
     }
   }
-
   //endregion
 
   @override
@@ -188,8 +188,7 @@ class _NewPostPageState extends State<NewPostPage> {
               child: dataIsAudio
                   ? InkWell(
                       onTap: () => print("Data is Audio"),
-                      child: SvgPicture.asset(
-                          "assets/images/logo/speaq_logo_white.svg"),
+                      child: SvgPicture.asset("assets/images/logo/speaq_logo_white.svg"),
                     )
                   : Image.file(
                       _imageFile!,
@@ -199,8 +198,7 @@ class _NewPostPageState extends State<NewPostPage> {
             Align(
               alignment: Alignment.centerRight,
               child: IconButton(
-                icon: const Icon(Icons.delete_forever_rounded,
-                    color: spqErrorRed),
+                icon: const Icon(Icons.delete_forever_rounded, color: spqErrorRed),
                 onPressed: () {
                   setState(
                     () {
@@ -248,8 +246,7 @@ class _NewPostPageState extends State<NewPostPage> {
                     ),
                   ),
                   hintText: 'Speaq',
-                  contentPadding: const EdgeInsets.only(
-                      left: 16.0, bottom: 8.0, top: 8.0, right: 16.0),
+                  contentPadding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 8.0, right: 16.0),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(50.0),
                   ),
@@ -339,8 +336,7 @@ class _NewPostPageState extends State<NewPostPage> {
   onEmojiSelected(Emoji emoji) {
     _postController
       ..text += emoji.emoji
-      ..selection = TextSelection.fromPosition(
-          TextPosition(offset: _postController.text.length));
+      ..selection = TextSelection.fromPosition(TextPosition(offset: _postController.text.length));
   }
 
   _onBackspacePressed() {
@@ -391,14 +387,12 @@ class _NewPostPageState extends State<NewPostPage> {
           );
         },
         child: Container(
-          decoration: BoxDecoration(
-              color: spqLightGrey, borderRadius: BorderRadius.circular(5)),
+          decoration: BoxDecoration(color: spqLightGrey, borderRadius: BorderRadius.circular(5)),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(5),
             child: SizedBox.fromSize(
               size: Size.fromRadius(32), // Image radius
-              child:
-                  Image.asset('assets/images/gallery.png', fit: BoxFit.cover),
+              child: Image.asset('assets/images/gallery.png', fit: BoxFit.cover),
             ),
           ),
         ),
@@ -441,9 +435,7 @@ class _NewPostPageState extends State<NewPostPage> {
             borderRadius: BorderRadius.circular(5),
             child: SizedBox.fromSize(
               size: Size.fromRadius(32), // Image radius
-              child: const Image(
-                  image: AssetImage('assets/images/camera.png'),
-                  fit: BoxFit.cover),
+              child: const Image(image: AssetImage('assets/images/camera.png'), fit: BoxFit.cover),
             ),
           ),
         ),
@@ -511,16 +503,10 @@ class _NewPostPageState extends State<NewPostPage> {
   void _sendResource() {
     //TODO "Resource Upload in Bloc intigrieren"
     //_resourceBloc.add(SaveResource());
-
   }
 
   void _createPost() {
-    Post _post = Post(
-        date: dateNow,
-        description: _postController.text,
-        resourceID: 1,
-        id: 1,
-        ownerID: widget.userID);
+    Post _post = Post(date: dateNow, description: _postController.text, resourceID: 1, id: 1, ownerID: widget.userID);
     _postBloc.add(CreatePost(ownerId: widget.userID, post: _post));
   }
 
@@ -541,15 +527,11 @@ class _NewPostPageState extends State<NewPostPage> {
               StreamBuilder<RecordingDisposition>(
                 stream: recorder.onProgress,
                 builder: (context, snapshot) {
-                  final duration = snapshot.hasData
-                      ? snapshot.data!.duration
-                      : Duration.zero;
+                  final duration = snapshot.hasData ? snapshot.data!.duration : Duration.zero;
 
                   String twoDigits(int n) => n.toString().padLeft(2, "0");
-                  String twoDigitMinutes =
-                      twoDigits(duration.inMinutes.remainder(60));
-                  String twoDigitSeconds =
-                      twoDigits(duration.inSeconds.remainder(60));
+                  String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+                  String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
 
                   return Text(
                     '$twoDigitMinutes:$twoDigitSeconds',
@@ -567,8 +549,7 @@ class _NewPostPageState extends State<NewPostPage> {
   Widget buildAudioButtonFunction() {
     return ElevatedButton(
       child: Icon(recorder.isRecording ? Icons.stop : Icons.mic, size: 32),
-      style: ElevatedButton.styleFrom(
-          fixedSize: Size(132, 132), shape: CircleBorder()),
+      style: ElevatedButton.styleFrom(fixedSize: Size(132, 132), shape: CircleBorder()),
       onPressed: () async {
         if (recorder.isRecording) {
           await stop();
